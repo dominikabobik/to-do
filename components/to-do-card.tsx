dccffcccbd93f8fb2ef6043ba743faf231defcbd
Dominika Bobik <dominikabobik12@gmail.com>
2022-10-04 14:13:49 -0400

    update

--- components/to-do-card.tsx
+++ components/to-do-card.tsx
@@ -1,38 +1,117 @@
-import type { NextPage } from 'next'
 import styles from '../styles/Todo.module.css'
-import { Checkbox } from '@nextui-org/react';
+import { useState } from 'react';
+import { AiOutlineDelete } from 'react-icons/ai'
+import { IconContext } from 'react-icons';
+import TextareaAutosize from 'react-textarea-autosize';
 
 export interface Item {
   title: string,
-  data: string[]
+  data: ListItemProps[]
 }
 
-const data1: string[] = [
-  'one',
-  'two',
-  'threesdkhj,msdbfkbskdjfbkdsbfksdbfkjsbdkjfbskjbfkjsdbkfjdbfbksdfbkjdsbkfjbsdbfsdkfbksjbkfbdskjD',
-  'sbfjhbs djfbjsdb fkjbsdkfjb skj fbkdjsbfkjsbkjdb fksbd dmfsdfbskjfksdkfbs dkb  f ksdbfkjbd',
-  'sdf',
-  'DaDom',
-  'hiiiii'
-];
+interface ListItemProps {
+  id: string;
+  value: string;
+  isChecked: boolean;
+  onClick?: () => void;
+  onDeleteClick?: () => void;
+  onInputChange?: () => void;
+}
 
-const x: Item = {title: 'Hello', data: data1}
- 
+
 const ToDoCard = () => {
+
+  const [items, setItems] = useState<ListItemProps[]>([])
+  const [listName, setListName] = useState('Title')
+
+  // Function to generate random Ids
+  function randomId(): string {
+    return Math.random().toString(36).slice(2, 7)
+  }
+
+  const onListNameChangeHandler = (event: any) => {
+    setListName(event.target.value);
+  }
+
+  const ListItem = (props: ListItemProps) => {
+    const [value, setValue] = useState(props.value)
+
+    const onValueChangeHandler = (event: any) => {
+      setValue(event.target.value);
+      // Update global data list
+      items.forEach(e => {
+        if (e.id === props.id) {
+          e.value = event.target.value;
+        }
+      })
+      console.log("setting value")
+      setItems(items)
+    };
+
+    return (
+      <div className={styles.listItem + (props.isChecked ? (" " + styles.isListItemChecked) : "")}>
+        <button className={styles.listItemButton} onClick={props.onClick}></button>
+        <TextareaAutosize value={value} onChange={onValueChangeHandler}
+          className={styles.listItemText + (props.isChecked ? (" " + styles.isListItemTextChecked) : "")}>
+        </TextareaAutosize>
+        <button className={styles.deleteButton} onClick={props.onDeleteClick}>
+          <IconContext.Provider value={{ className: "delete" }}>
+            <AiOutlineDelete></AiOutlineDelete>
+          </IconContext.Provider>
+        </button>
+      </div>
+    )
+  }
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
-        <p className={styles.listName}>{x.title}</p>
-        <ul className={styles.list}>{x.data.map((item: string, i) =>
-            <Checkbox disableAnimation={true}
-              defaultSelected={true}
-              lineThrough={true}
-              isRounded={true}
-              className={styles.listItem} key={i}>
-              <span className={styles.listItemText}>{`${item}`}</span>
-            </Checkbox>
-           )}</ul>
+        <div className={styles.topWrapper}>
+          <TextareaAutosize
+            value={listName}
+            onChange={onListNameChangeHandler}
+            className={styles.listName}>
+          </TextareaAutosize>
+          <button
+            className={styles.addButton}
+            onClick={() => {
+              items.push({
+                id: randomId(),
+                value: '',
+                isChecked: false,
+              })
+              setItems([...items])
+            }}>+
+          </button>
+        </div>
+
+
+        <ul className={styles.list}>
+          {items.sort((a, b) => {
+            const aChecked = a ? 1 : -1
+            const bChecked = b ? 1 : -1
+            return bChecked - aChecked
+          }).map((item, i) =>
+            <ListItem
+              id={item.id}
+              value={item.value}
+              isChecked={item.isChecked}
+              key={i + item.value}
+              onClick={() => {
+                for (const i of items) {
+                  if (i.id === item.id) {
+                    i.isChecked = !i.isChecked
+                    setItems([...items])
+                    return
+                  }
+                }
+              }} onDeleteClick={() => {
+                items.splice(i, 1)
+                setItems([...items])
+              }} onInputChange={() => {
+
+              }} />
+          )}</ul>
       </div>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
+import { Db } from 'mongodb'
 import type { GetServerSideProps, NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { title } from 'process'
 import { useEffect, useState } from 'react'
+import { IconContext } from 'react-icons'
+import { AiOutlineDelete } from 'react-icons/ai'
 import TopBar from '../components/top-bar'
 import { connectToDatabase } from '../lib/mongodb'
 import styles from '../styles/Home.module.css'
+import TextareaAutosize from 'react-textarea-autosize';
 
 interface Props {
   items: {
@@ -14,45 +15,91 @@ interface Props {
   }[];
 }
 
-export const Home: NextPage<Props> = ({ items: initialItems }) => {
-  const [value, setValue] = useState('')
-  const [items, setItems] = useState(initialItems);
+interface ListItem {
+  id: string,
+  title: string
+}
 
-  const addFieldToList = () => {
-    setItems([{ id: 'something', title: value }, ...items])
+export const Home: NextPage = () => {
+
+
+  const [value, setValue] = useState('')
+  const [lists, setLists] = useState<ListItem[]>([])
+
+  // Function to generate random Ids
+  function randomId(): string {
+    return Math.random().toString(36).slice(2, 7)
   }
+
+  const ListItem = (props: ListItem) => {
+    const [name, setName] = useState(props.title)
+
+    const onDeleteClick = () => {
+      // Update global data list
+      lists.forEach((e, i) => {
+        if (e.id === props.id) {
+          lists.splice(i, 1)
+        }
+      })
+      console.log("deleting value")
+      setLists([...lists])
+    }
+
+    return (
+      <div className={styles.listItem} >
+        <div className={styles.listItemText}>{name}</div>
+        <button className={styles.deleteButton} onClick={onDeleteClick}>
+          <IconContext.Provider value={{ className: "delete" }}>
+            <AiOutlineDelete></AiOutlineDelete>
+          </IconContext.Provider>
+        </button>
+      </div>
+    )
+  }
+
 
   return (
     <div className={styles.container}>
-      <TopBar></TopBar>
-      <input placeholder='enter name'
-        onChange={(e) => setValue(e.target.value)}
-        value={value}></input>
-      <button className={styles.newButton} onClick={addFieldToList}>New Reminder</button>
-      <div className={styles.listContainer}>
-        <ul>{items.map(item =>
-          <div key={item.id}>{item.title}</div>
-        )}</ul>
+      <TopBar />
+      <div className={styles.wrapper}>
+        <div className={styles.topWrapper}>
+          <input placeholder='Title'
+            className={styles.newListName}
+            onChange={(e) => setValue(e.target.value)}
+            value={value}></input>
+          <button className={styles.newListButton}
+            onClick={() => {
+              if (value === '') return
+              setLists([{ id: randomId(), title: value }, ...lists])
+              setValue('')
+            }}>+</button>
+        </div>
+        <div className={styles.listContainer}>
+          <ul className={styles.list}>{lists.map((e, i) =>
+            <ListItem key={i} title={e.title} id={e.id}></ListItem>
+          )}</ul>
+        </div>
       </div>
+
     </div>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+// export const getServerSideProps: GetServerSideProps = async (context) => {
 
-  let { db } = await connectToDatabase();
-  let lists = await db
-    .collection('lists')
-    .find({})
-    .toArray();
+//   let { db } = await connectToDatabase();
+//   let lists = await db
+//     .collection('lists')
+//     .find({})
+//     .toArray();
 
-  return {
-    props: {
-      items: lists.map(item => {
-        return { id: item._id.toHexString(), title: item.name }
-      })
-    },
-  }
-}
+//   return {
+//     props: {
+//       items: lists.map(item => {
+//         return { id: item._id.toHexString(), title: item.name }
+//       })
+//     },
+//   }
+// }
 
 export default Home
