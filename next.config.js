cf6bec1b21e570e65ac0e145e31ce1005956c53e
Dominika Bobik <dominikabobik12@gmail.com>
2022-11-28 23:18:45 -0500

    pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA  = require("next-pwa");
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+  reactStrictMode: true
+});
 
-module.exports = nextConfig
 
 module.exports = {
   env: {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return <>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+      <meta name="description" content="description of your project" />
+      <meta name="theme-color" content="#000" />
+      <title>ToDo</title>
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="shortcut icon" href="/icon-192x192.png" />
+      <link rel="apple-touch-icon" href="/icon-512x512.png"></link>
+      <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
+    </Head>
+    <Component {...pageProps} />
+  </>
+
+
 }
 
 export default MyApp
